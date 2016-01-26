c5b1c600b43da04c22de82a14783cbb069bd311c
Mark <mark25@dom.hackafe.net>
2016-01-26 14:19:56 -0500

    Fixing sshkey grain

--- _grains/sshkey.py
+++ _grains/sshkey.py
@@ -2,5 +2,5 @@
 def sshkey():
   grains = {}
   with open("/etc/ssh/ssh_host_rsa_key.pub", "r") as sshkeyFile:
-    grains['sshkey'] = sshkeyFile.read()
+    grains['sshkey'] = sshkeyFile.read().split(" ")[1]
   return grains
