4e2a870bb0a2a7aab861d2e9431a9e59b9b60e2d
Mark <mark25@dom.hackafe.net>
2016-01-26 14:26:53 -0500

    Using ecdsa key instead

--- _grains/sshkey.py
+++ _grains/sshkey.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 def sshkey():
   grains = {}
-  with open("/etc/ssh/ssh_host_rsa_key.pub", "r") as sshkeyFile:
+  with open("/etc/ssh/ssh_host_ecdsa_key.pub", "r") as sshkeyFile:
     grains['sshkey'] = sshkeyFile.read()
   return grains
